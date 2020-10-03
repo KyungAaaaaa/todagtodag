cea7bb0e7b6be7f0acd15865c1a7f12b88f40210
jk77621 <jk77621@naver.com>
2020-10-03 18:11:57 +0900

    20201003 jjk2

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ include $_SERVER['DOCUMENT_ROOT'] . "/todagtodag/db/create_table.php";
 <head>
     <meta charset="utf-8">
     <title>토닥토닥</title>
-    <link rel="stylesheet" type="text/css" href="./css/common.css?ver=5">
+    <link rel="stylesheet" type="text/css" href="./css/common.css?ver=4">
+    <link rel="shortcut icon" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/todagtodag/img/todagtodag2.png">
+
 </head>
 
 <body>

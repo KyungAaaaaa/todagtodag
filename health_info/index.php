fb03c7d7ae097e35d74b848b2462dc1eb91c5318
kss7545 <kss7545@naver.com>
2020-10-03 18:57:29 +0900

    20201003_kss 세션문제 수정, 건강정보 페이지 가이드라인

---
+++ health_info/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>토닥토닥</title>
+	<link rel="stylesheet" type="text/css" href="http://<?= $_SERVER['HTTP_HOST'] ?>/todagtodag/css/common.css?ver=5">
+	<link rel="stylesheet" type="text/css" href="http://<?= $_SERVER['HTTP_HOST'] ?>/todagtodag/notice/css/notice.css">
+</head>
+
+<body>
+    <header>
+    <?php include  $_SERVER['DOCUMENT_ROOT']."/todagtodag/header.php"; ?>
+    </header>
+    <section>
+        <div>
+            <h1>건 강 정 보</h1>
+            총 x 개의 글
+        </div><br>
+        <div>
+            <input type="search" name="search" id="">
+        </div>
+        <div>태그 & 화살표</div>
+        <br><br><br>
+        <div>카테고리 버튼</div>
+        <br>
+        많이 찾는 건강 정보
+        <div>이미지 게시판</div>
+        <br>
+        <div>더보기 버튼(모든 리스트)</div>
+
+
+
+
+
+    </section>
+    <footer>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/todagtodag/footer.php"; ?>
+    </footer>
+</body>
+
+</html>

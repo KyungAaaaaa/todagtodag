2ea0da8d6f3f1d837645d6eb4fe12db14055d17e
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-10-12 13:35:15 +0900

    20201012 kka
    메인화면 건강정보 출력

--- home/home.php
+++ home/home.php
@@ -41,7 +41,40 @@
 	</div>
 	<div class="today_health_info">
 		<h1>오늘의 건강 정보</h1>
-		<div></div>
+		<div class="content">
+            <?
+                $query = "select num from health_info";
+                $result = $con->query($query) or die($con);
+                if ($num_rows = mysqli_num_rows($result) !== 0){
+                $health_info_num = [];
+                while ($row = mysqli_fetch_row($result)) {
+                    array_push($health_info_num, $row[0]);
+                }
+                $array = [];
+                for($i=0;$i<5;$i++){
+                    array_push($array, $i);
+                }
+                $num = array_rand($health_info_num);
+                $query = "select * from health_info where num={$health_info_num[$num]}";
+                $result = $con->query($query) or die($con);
+                $row = mysqli_fetch_assoc($result);
+                $num = $row['num'];
+                $subject = $row['subject'];
+                $content = $row['content'];
+                $hit = $row['hit'] + 1;
+                $file_name = $row['file_name_1'];
+                $file_copied = $row['file_copied_1'];
+                $file_type = $row['file_type_1'];
+                $root = "http://" . $_SERVER['HTTP_HOST'] . "/todagtodag";
+                if (strpos($file_type, "image") !== false) echo "<img src='{$root}/health_info/data/$file_copied'>";
+                else echo "<img src='{$root}/hospital/img/hospital.png'>";
+            ?>
+			<div><h2><?= $subject ?></h2>
+				<p><?= $content ?></p>
+				<span><a href="http://<?= $_SERVER['HTTP_HOST'] ?>/todagtodag/health_info/health_info_view.php?num=<?= $num ?>&hit=<?= $hit ?>"><h4>더보기 ></h4></a></span>
+			</div>
+		</div>
+        <? } ?>
 	</div>
 	<div class="notice">
 		<h1>공지사항</h1>

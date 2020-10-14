d52fd0edc7d75cc961c6a15afb7da69197023ed1
kss7545 <kss7545@naver.com>
2020-10-14 17:48:57 +0900

    20201021 kss 자유게시판 오류수정, 덧글추가

--- board/free/free_view.php
+++ board/free/free_view.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -26,6 +27,7 @@
 
 			include_once $_SERVER['DOCUMENT_ROOT'] . "/todagtodag/db/db_connector.php";
 			include_once $_SERVER['DOCUMENT_ROOT'] . "/todagtodag/db/create_table.php";
+			include $_SERVER['DOCUMENT_ROOT']."/todagtodag/board/free/free_func.php";
 
 			create_table($con, 'free');
 			create_table($con, 'free_ripple');
@@ -48,9 +50,25 @@
 			$content = str_replace(" ", "&nbsp;", $content);
 			$content = str_replace("\n", "<br>", $content);
 
+			$type = explode("/", $file_type);
+			//숫자 0 " " '0' null 0.0   $a = array()
+			if(!empty($file_copied)&&$type[0] =="image"){
+				//이미지 정보를 가져오기 위한 함수 width, height, type
+				$image_info=getimagesize("./data/".$file_copied);
+				$image_width=$image_info[0];
+				$image_height=$image_info[1];
+				$image_type=$image_info[2];
+				if($image_width>400) $image_width = 400;
+			  }else{
+				$image_width=0;
+				$image_height=0;
+				$image_type="";
+			  }
+
 			$new_hit = $hit + 1;
 			$sql = "update free set hit=$new_hit where num=$num";
-			mysqli_query($con, $sql);
+			mysqli_query($con,$sql);
+
 			?>
 			<ul id="view_content">
 				<li>
@@ -58,18 +76,61 @@
 					<span class="col2"><?= $name ?> | <?= $regist_day ?></span>
 				</li>
 				<li>
-					<?php
-					if ($file_name) {
-						$real_name = $file_copied;
-						$file_path = "./data/" . $real_name;
-						$file_size = filesize($file_path); //php 함수
-
-						echo "▷ 첨부파일 : $file_name ($file_size Byte) &nbsp;&nbsp;&nbsp;&nbsp;
-			       		<a href='free_download.php?num=$num&real_name=$real_name&file_name=$file_name&file_type=$file_type'>[저장]</a><br><br>";
-					}
-					?>
+				<?php
+                    if($type[0] =="image"){
+                      echo "<img src='./data/$file_copied' width='$image_width'><br>";
+                    }elseif(!empty($_SESSION['user_id'])&&!empty($file_copied)){
+					  $real_name = $file_copied;
+                      $file_path = "./data/".$real_name;
+                      $file_size = filesize($file_path);
+                      //2. 업로드된 이름을 보여주고 [저장] 할것인지 선택한다.
+                      echo "
+                        ▷ 첨부파일 : $file_name &nbsp; [ $file_size Byte ]
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href='free_download.php?num=$num&real_name=$real_name&file_name=$file_name&file_type=$file_type'>[저장]</a><br><br>";
+                    }
+                  ?>
 					<?= $content ?>
 				</li>
+			</ul>
+			<ul id="ripple">
+					<?php
+					$sql="select * from free_ripple where parent='$num' ";
+					$ripple_result= mysqli_query($con,$sql);
+					while($ripple_row=mysqli_fetch_array($ripple_result)){
+					  $ripple_num=$ripple_row['num'];
+					  $ripple_id=$ripple_row['id'];
+					  $ripple_name=$ripple_row['name'];
+					  $ripple_date=$ripple_row['regist_day'];
+					  $ripple_content=$ripple_row['content'];
+					  $ripple_content=str_replace("\n", "<br>",$ripple_content);
+					  $ripple_content=str_replace(" ", "&nbsp;",$ripple_content);
+					?>
+					<li><?=$ripple_id."&nbsp;&nbsp;".$ripple_date?></li>
+            <li id="mdi_del">
+            <?php
+            $message =free_ripple_delete($ripple_id,$ripple_num,'dmi_free.php',$page,$hit,$num);
+            echo $message;
+            ?>
+			</li>
+			<li id="ripple_content">
+			<?=$ripple_content?>
+			</li>
+			<?php
+      }//end of while
+      mysqli_close($con);
+	?>
+	<form name="ripple_form" action="dmi_free.php?mode=insert_ripple" method="post">
+      <input type="hidden" name="parent" value="<?=$num?>">
+      <input type="hidden" name="hit" value="<?=$hit?>">
+      <input type="hidden" name="page" value="<?=$page?>">
+      <div id="ripple_insert">
+        <div id="ripple_textarea"><textarea name="ripple_content" rows="3" cols="80"></textarea></div>
+        <div id="ripple_button"> <input type="image"  src="./img/memo_ripple_button.png"></div>
+      </div><!--end of ripple_insert -->
+    </form>
+  </div><!--end of ripple2  -->
+</div><!--end of ripple  -->
 			</ul>
 			<ul class="buttons">
 				<li><button onclick="location.href='free_list.php?page=<?= $page ?>'">목록</button></li>
